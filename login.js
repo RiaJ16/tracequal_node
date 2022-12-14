8fd0629a07847ca46d81cfdcafabc25d8325d679
Jair Nájera <jair.najera@htech.mx>
2022-12-14 16:01:00 -0600

    login completo

--- login.js
+++ login.js
@@ -1,8 +1,28 @@
+const models = require('./models')
+
 module.exports = function(app){
     const ruta_login = '/login'
 
     app.post(ruta_login, (req, res)=>{
-        console.log("intento de login")
+        username = req.body.username;
+        password = req.body.password;
+        if(username && password){
+            models.Usuario.find()
+            .then(doc=>{
+                if(password == doc[0].password){
+                    res.json({response:'Satisfactorio', login: true})
+                }
+                else{
+                    res.json({response:'No satisfactorio', login: false})
+                }
+            })
+            .catch(err=>{
+                console.log('Error: ' + err)
+            })
+        }
+        else{
+            res.json({response:'No satisfactorio', login: false})
+        }
     })
 
 }
